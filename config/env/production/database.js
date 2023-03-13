f5e3bd308b175f991a706cd81077854eaae6b9e2
trxsh1 <yagosousa630@gmail.com>
2023-03-13 14:29:42 -0300

    test final

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,22 @@
-// // path: ./config/env/production/database.js
+// path: ./config/env/production/database.js
 
-// const { parse } = require("pg-connection-string");
+const { parse } = require("pg-connection-string");
 
-// module.exports = ({ env }) => {
-//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
-//   return {
-//     connection: {
-//       client: 'postgres',
-//       connection: {
-//         host,
-//         port,
-//         database,
-//         user,
-//         password,
-//         ssl: { rejectUnauthorized: false },
-//       },
-//       debug: false,
-//     },
-//   }
-// };
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
+      },
+      debug: false,
+    },
+  }
+};
